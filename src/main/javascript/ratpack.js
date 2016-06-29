de24bd4919a047b0cc8c9dc81c1ff791f5358c91
Colin Harrington <colin.harrington@gmail.com>
2016-06-29 16:29:09 -0500

    Craigified addressing half of #1

--- src/main/javascript/ratpack.js
+++ src/main/javascript/ratpack.js
@@ -1,5 +1,16 @@
+var ScriptEngine = Java.type('javax.script.ScriptEngine');
+var QuercusScriptEngineFactory = Java.type('com.caucho.quercus.script.QuercusScriptEngineFactory');
+var StringWriter = Java.type('java.io.StringWriter');
+var PrintWriter = Java.type('java.io.PrintWriter');
+
 ratpack.handlers(function(chain) {
     chain.get(function(ctx) {
-        ctx.render("Hello World!");
+        var factory = new QuercusScriptEngineFactory();
+        var engine = factory.getScriptEngine();
+        var sw = new StringWriter();
+        var pw = new PrintWriter(sw);
+        engine.getContext().setWriter(pw);
+        engine.eval('<?php echo "NEEDS PHP FOR BUILD AND THE APP"; ?>')
+        ctx.render(sw.getBuffer());
     })
 });
